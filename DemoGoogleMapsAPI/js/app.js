d08bdcf0766d3b353d02e1ba9b9d6326df6ac97c
Jonatan Michiels <jonatan.michiels@student.howest.be>
2015-10-28 18:36:00 +0100

    Demo maps toegevoegd
    Hoe toon ik een kaart :+1:

---
+++ DemoGoogleMapsAPI/js/app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Jonatan on 28/10/2015.
+ */
+
+function initMap() {
+    var map = new google.maps.Map(document.querySelector("#map"), {
+        center: {lat: 51.1793027, lng: 3.08195},
+        zoom: 15
+    });
+}
