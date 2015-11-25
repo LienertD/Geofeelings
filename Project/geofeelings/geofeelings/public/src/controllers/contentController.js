0c7b64481310b03e278e292d87ba4fe0a0e1f742
Jonatan Michiels <jonatan.michiels@student.howest.be>
2015-11-25 20:29:54 +0100

    Geofeelings checkboxen en slider
    Checkboxen => Afgewerkt
    Slider => Afgewerkt

---
+++ Project/geofeelings/geofeelings/public/src/controllers/contentController.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Jonatan on 25/11/2015.
+ */
+
+
+(function () {
+    "use strict";
+
+    var contentController = function ($scope) {
+
+    };
+
+    angular.module("geofeelings").controller("contentController", ["$scope", contentController]);
+})();
