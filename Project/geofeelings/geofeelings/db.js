54a430c5e32b2221bf07ee65005889bb470d9eaf
LienertD <lienert.deprez@gmail.com>
2015-12-09 17:55:46 +0100

    Opkuisen BE + start connect met db

--- Project/geofeelings/geofeelings/db.js
+++ Project/geofeelings/geofeelings/db.js
@@ -1,4 +1,30 @@
-module.exports = {
-	'url' : 'mongodb://lienert:123456@ds053874.mongolab.com:53874/geofeelingz'
-	//'url' : 'mongodb://localhost/test'
-};
+(function (database){
+	var mongodb = require("mongodb");
+	var mongourl = 'mongodb://lienert:123456@ds053874.mongolab.com:53874/geofeelingz';
+	var thedb = null;
+
+	database.getDB = function(next){
+		if(!thedb)
+		{
+			//verbinden met db
+			mongodb.MongoClient.connect(mongoUrl, function(err, db)
+			{
+				if(err){
+					next(err, null);
+				}
+				else
+				{
+					thedb = {
+						db:db
+					};
+
+					next(null, thedb);
+				}
+			});
+		}
+		else
+		{
+			next(null, thedb);
+		}
+	}
+});
